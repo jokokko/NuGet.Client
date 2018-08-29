4ad0287dbd1804a3f9c52e29a15c264dad51a979
AbdallahAbuHashem <aabuhash@stanford.edu>
2018-08-29 13:46:41 -0700

    Added some tests

--- test/NuGet.Core.FuncTests/NuGet.XPlat.FuncTest/XplatListPkgTests.cs
+++ test/NuGet.Core.FuncTests/NuGet.XPlat.FuncTest/XplatListPkgTests.cs
@@ -21,49 +21,189 @@ namespace NuGet.XPlat.FuncTest
         private static MSBuildAPIUtility MsBuild => new MSBuildAPIUtility(new TestCommandOutputLogger());
 
         [Fact]
-        public void ListPkg()
-        {
-            // Arrange
-            var projectPath = Path.Combine(Path.GetTempPath(), "project_foo.csproj");
-            File.Create(projectPath).Dispose();
-            var argList = new List<string>() {
-                "list",
-                projectPath,
-                "package"};
-
-            var logger = new TestCommandOutputLogger();
-            var testApp = new CommandLineApplication();
-            var mockCommandRunner = new Mock<IListPackageCommandRunner>();
-            mockCommandRunner
-                .Setup(m => m.ExecuteCommandAsync(It.IsAny<ListPackageArgs>()));
-            testApp.Name = "dotnet nuget_test";
-
-            ListPackageCommand.Register(testApp,
-                () => logger,
-                () => mockCommandRunner.Object);
-
-
-            // Act
-            var result = testApp.Execute(argList.ToArray());
-    
-
-            XPlatTestUtils.DisposeTemporaryFile(projectPath);
-
-            // Assert
-            //mockCommandRunner.Verify(m => m.ExecuteCommandAsync(It.Is<ListPackageArgs>(p =>
-            //p.PackageDependency.Id == package &&
-            //p.ProjectPath == projectPath)));
-
-            Assert.Equal(0, result);
-        }
-
-        [Fact]
-        public async void DotnetListPackage_Outdated_Succeed()
+        public async void DotnetListPackage_NoRestore_Fail()
         {
             using (var pathContext = new SimpleTestPathContext())
             {
                 var projectA = XPlatTestUtils.CreateProject(ProjectName, pathContext, "net46");
-                var versions = new List<string> { "1.0.0", "1.0.9", "1.9.0", "2.1.0", "2.2.0-beta" };
+
+                var packageX = XPlatTestUtils.CreatePackage();
+
+                // Generate Package
+                await SimpleTestPackageUtility.CreateFolderFeedV3Async(
+                    pathContext.PackageSource,
+                    PackageSaveMode.Defaultv3,
+                    packageX);
+
+
+                var buildFixture = new XPlatMsbuildTestFixture();
+                var addResult = CommandRunner.Run(buildFixture._dotnetCli,
+                    Directory.GetParent(projectA.ProjectPath).FullName,
+                    $"add {projectA.ProjectPath} package packageX --no-restore",
+                    waitForExit: true);
+                Assert.True(addResult.Success);
+
+                var listResult = CommandRunner.Run(buildFixture._dotnetCli,
+                    Directory.GetParent(projectA.ProjectPath).FullName,
+                    $"list {projectA.ProjectPath} package",
+                    waitForExit: true);
+
+                Assert.True(ContainsIgnoringSpaces(listResult.AllOutput, "No assets file was found".Replace(" ", "")));
+            }
+        }
+
+        [Fact]
+        public async void DotnetListPackage_Transitive()
+        {
+            using (var pathContext = new SimpleTestPathContext())
+            {
+                var projectA = XPlatTestUtils.CreateProject(ProjectName, pathContext, "net46");
+
+                var packageX = XPlatTestUtils.CreatePackage();
+                var packageY = XPlatTestUtils.CreatePackage(packageId: "packageY");
+                packageX.Dependencies.Add(packageY);
+
+                // Generate Package
+                await SimpleTestPackageUtility.CreateFolderFeedV3Async(
+                    pathContext.PackageSource,
+                    PackageSaveMode.Defaultv3,
+                    packageX,
+                    packageY);
+
+
+                var buildFixture = new XPlatMsbuildTestFixture();
+                var addResult = CommandRunner.Run(buildFixture._dotnetCli,
+                    Directory.GetParent(projectA.ProjectPath).FullName,
+                    $"add {projectA.ProjectPath} package packageX --no-restore",
+                    waitForExit: true);
+                Assert.True(addResult.Success);
+
+                var restoreResult = CommandRunner.Run(buildFixture._dotnetCli,
+                    Directory.GetParent(projectA.ProjectPath).FullName,
+                    $"restore {projectA.ProjectName}.csproj",
+                    waitForExit: true);
+                Assert.True(restoreResult.Success);
+
+                var listResult = CommandRunner.Run(buildFixture._dotnetCli,
+                    Directory.GetParent(projectA.ProjectPath).FullName,
+                    $"list {projectA.ProjectPath} package",
+                    waitForExit: true);
+
+                Assert.False(ContainsIgnoringSpaces(listResult.AllOutput, "packageY"));
+
+                listResult = CommandRunner.Run(buildFixture._dotnetCli,
+                    Directory.GetParent(projectA.ProjectPath).FullName,
+                    $"list {projectA.ProjectPath} package --include-transitive",
+                    waitForExit: true);
+
+                Assert.True(ContainsIgnoringSpaces(listResult.AllOutput, "packageY"));
+
+            }
+        }
+
+        [Theory]
+        [InlineData("", "[.NETFramework,Version=v4.6]", null)]
+        [InlineData("", "[.NETFramework,Version=v4.5.1]", null)]
+        [InlineData("--framework net451 --framework net46", "[.NETFramework,Version=v4.6]", null)]
+        [InlineData("--framework net451 --framework net46", "[.NETFramework,Version=v4.5.1]", null)]
+        [InlineData("--framework net451", "[.NETFramework,Version=v4.5.1]", "[.NETFramework,Version=v4.6]")]
+        [InlineData("--framework net46", "[.NETFramework,Version=v4.6]", "[.NETFramework,Version=v4.5.1]")]
+        public async void DotnetListPackage_FrameworkSpecific_Success(string args, string shouldInclude, string shouldntInclude)
+        {
+            using (var pathContext = new SimpleTestPathContext())
+            {
+                var projectA = XPlatTestUtils.CreateProject(ProjectName, pathContext, "net46;net451");
+
+                var packageX = XPlatTestUtils.CreatePackage(frameworkString: "net46;net451");
+
+                // Generate Package
+                await SimpleTestPackageUtility.CreateFolderFeedV3Async(
+                    pathContext.PackageSource,
+                    PackageSaveMode.Defaultv3,
+                    packageX);
+
+
+                var buildFixture = new XPlatMsbuildTestFixture();
+                var addResult = CommandRunner.Run(buildFixture._dotnetCli,
+                    Directory.GetParent(projectA.ProjectPath).FullName,
+                    $"add {projectA.ProjectPath} package packageX --no-restore",
+                    waitForExit: true);
+                Assert.True(addResult.Success);
+
+                var restoreResult = CommandRunner.Run(buildFixture._dotnetCli,
+                    Directory.GetParent(projectA.ProjectPath).FullName,
+                    $"restore {projectA.ProjectName}.csproj",
+                    waitForExit: true);
+                Assert.True(restoreResult.Success);
+
+                var listResult = CommandRunner.Run(buildFixture._dotnetCli,
+                    Directory.GetParent(projectA.ProjectPath).FullName,
+                    $"list {projectA.ProjectPath} package {args}",
+                    waitForExit: true);
+
+                Assert.True(ContainsIgnoringSpaces(listResult.AllOutput, shouldInclude.Replace(" ", "")));
+
+                if (shouldntInclude != null)
+                {
+                    Assert.False(ContainsIgnoringSpaces(listResult.AllOutput, shouldntInclude.Replace(" ", "")));
+                }
+
+            }
+        }
+
+        [Fact]
+        public async void DotnetListPackage_InvalidFramework_Fail()
+        {
+            using (var pathContext = new SimpleTestPathContext())
+            {
+                var projectA = XPlatTestUtils.CreateProject(ProjectName, pathContext, "net46");
+                
+                var packageX = XPlatTestUtils.CreatePackage();
+
+                // Generate Package
+                await SimpleTestPackageUtility.CreateFolderFeedV3Async(
+                    pathContext.PackageSource,
+                    PackageSaveMode.Defaultv3,
+                    packageX);
+
+
+                var buildFixture = new XPlatMsbuildTestFixture();
+                var addResult = CommandRunner.Run(buildFixture._dotnetCli,
+                    Directory.GetParent(projectA.ProjectPath).FullName,
+                    $"add {projectA.ProjectPath} package packageX --no-restore",
+                    waitForExit: true);
+                Assert.True(addResult.Success);
+
+                var restoreResult = CommandRunner.Run(buildFixture._dotnetCli,
+                    Directory.GetParent(projectA.ProjectPath).FullName,
+                    $"restore {projectA.ProjectName}.csproj",
+                    waitForExit: true);
+                Assert.True(restoreResult.Success);
+
+                var listResult = CommandRunner.Run(buildFixture._dotnetCli,
+                    Directory.GetParent(projectA.ProjectPath).FullName,
+                    $"list {projectA.ProjectPath} package --framework net46 --framework invalidFramework",
+                    waitForExit: true);
+
+                Assert.False(listResult.Success);
+
+            }
+        }
+
+        [Theory]
+        [InlineData("1.0.0", "", "2.1.0")]
+        [InlineData("1.0.0", "--include-prerelease", "2.2.0-beta")]
+        [InlineData("1.0.0-beta", "", "2.2.0-beta")]
+        [InlineData("1.0.0", "--highest-patch", "1.0.9")]
+        [InlineData("1.0.0", "--highest-minor", "1.9.0")]
+        [InlineData("1.0.0", "--highest-patch --include-prerelease", "1.0.10-beta")]
+        [InlineData("1.0.0", "--highest-minor --include-prerelease", "1.10.0-beta")]
+        public async void DotnetListPackage_Outdated_Succeed(string currentVersion, string args, string expectedVersion)
+        {
+            using (var pathContext = new SimpleTestPathContext())
+            {
+                var projectA = XPlatTestUtils.CreateProject(ProjectName, pathContext, "net46");
+                var versions = new List<string> { "1.0.0-beta", "1.0.0", "1.0.9", "1.0.10-beta", "1.9.0", "1.10.0-beta", "2.1.0", "2.2.0-beta" };
                 foreach (var version in versions)
                 {
                     var packageX = XPlatTestUtils.CreatePackage(packageId: "packageX", packageVersion: version);
@@ -77,35 +217,24 @@ namespace NuGet.XPlat.FuncTest
 
 
                 var buildFixture = new XPlatMsbuildTestFixture();
-                var result = CommandRunner.Run(buildFixture._dotnetCli,
+                var addResult = CommandRunner.Run(buildFixture._dotnetCli,
                     Directory.GetParent(projectA.ProjectPath).FullName,
-                    $"add {projectA.ProjectPath} package packageX --version 1.0.0",
+                    $"add {projectA.ProjectPath} package packageX --version {currentVersion} --no-restore",
                     waitForExit: true);
+                Assert.True(addResult.Success);
 
-                //var argList = new List<string>() {
-                //        "add", "--package", "packageX", "--version",
-                //        "1.0.0",
-                //        "--project",
-                //        projectA.ProjectPath,
-                //        "--no-restore"};
-
-                //var logger = new TestCommandOutputLogger();
-                //var testApp = new CommandLineApplication();
-                //var mockCommandRunner = new Mock<IPackageReferenceCommandRunner>();
-                //mockCommandRunner
-                //    .Setup(m => m.ExecuteCommand(It.IsAny<PackageReferenceArgs>(), It.IsAny<MSBuildAPIUtility>()))
-                //    .ReturnsAsync(0);
-
-                //testApp.Name = "dotnet nuget_test";
-                //AddPackageReferenceCommand.Register(testApp,
-                //    () => logger,
-                //    () => mockCommandRunner.Object);
-                //var result = testApp.Execute(argList.ToArray());
-
-                var resultt = CommandRunner.Run(buildFixture._dotnetCli,
+                var restoreResult = CommandRunner.Run(buildFixture._dotnetCli,
                     Directory.GetParent(projectA.ProjectPath).FullName,
                     $"restore {projectA.ProjectName}.csproj",
                     waitForExit: true);
+                Assert.True(restoreResult.Success);
+
+                var listResult = CommandRunner.Run(buildFixture._dotnetCli,
+                    Directory.GetParent(projectA.ProjectPath).FullName,
+                    $"list {projectA.ProjectPath} package --outdated {args}",
+                    waitForExit: true);
+                
+                Assert.True(ContainsIgnoringSpaces(listResult.AllOutput, $"packageX{currentVersion}{currentVersion}{expectedVersion}"));
 
             }
         }
@@ -114,7 +243,7 @@ namespace NuGet.XPlat.FuncTest
         {
             var commandResultNoSpaces = output.Replace(" ", "");
 
-            return commandResultNoSpaces.Contains(pattern);
+            return commandResultNoSpaces.ToLowerInvariant().Contains(pattern.ToLowerInvariant());
         }
     }
 }
